1ad2420907c9eecdfd353537f23edd76eae86790
Stanislav Maslov <smaslov@myseneca.ca>
2023-08-18 20:59:44 -0400

    Updated javascript and html

--- js/app.js
+++ js/app.js
@@ -2,3 +2,8 @@ function showRate() {
   var rate = document.getElementById("hourlyRate");
   rate.style.display = "block";
 }
+
+function hideRate() {
+  var rate = document.getElementById("hourlyRate");
+  rate.style.display = "none";
+}
